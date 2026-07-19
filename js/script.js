df7b78af0b96042291f118a34f8b191d72ac7923
fareeddy <fareeddyahmed@gmail.com>
2026-07-19 18:04:54 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+alert("javaScript is working!")
+const menuToggle =document.getElementById("menuToggle");
+
+const navLinks =document.getElementById("navLinks");
+menuToggle.addEventListener("click", function() {
+    navLinks.classList.toggle("show");
+})
+
+const fadeElements =document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+fadeElements.forEach(element =>{
+    observer.observe(element);
+});
